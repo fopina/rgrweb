141985f4284526cdc065fefe90ca42ecf4dabb88
Filipe Pina <fopina@gmail.com>
2020-11-18 00:58:02 +0000

    added token authorization (and arg to disable it)

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/fopina/rgrweb/assets"
@@ -17,12 +20,15 @@ var gpioIn = flag.IntP("pin-in", "i", 0, "Input GPIO (feedback) - 0 means fake i
 var gpioOut = flag.IntP("pin-out", "o", 0, "Output GPIO (trigger) - 0 means fake it")
 var testInput = flag.Bool("test-input", false, "Reading input GPIO for 5 seconds (testing)")
 var testOutput = flag.Bool("test-output", false, "Enable output GPIO for 5 seconds (testing)")
+var noAuthRequired = flag.Bool("no-auth", false, "Start without any tokens configured (hopefully testing only!)")
+var tokens = flag.StringArrayP("token", "t", nil, "Tokens required for authentication, format LOGGING_ID:TOKEN")
+var tokenFile = flag.String("token-file", "", "File containing list of tokens, format LOGGING_ID:TOKEN, one per line")
 var highDuration = flag.DurationP("duration", "d", 5*time.Second, "Time that output GPIO pin will be HIGH")
 
-func runIt() {
+func runIt() error {
 	err := gpio.SetUp(*gpioIn, *gpioOut)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer gpio.Cleanup()
 
@@ -44,13 +50,52 @@ func runIt() {
 		gpio.SetTrigger(true)
 		time.Sleep(*highDuration)
 		gpio.SetTrigger(false)
-		return
+		return nil
+	}
+
+	if *tokenFile != "" {
+		file, err := os.Open(*tokenFile)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			*tokens = append(*tokens, scanner.Text())
+		}
+		if err := scanner.Err(); err != nil {
+			return err
+		}
+	}
+
+	if !*noAuthRequired && len(*tokens) == 0 {
+		return fmt.Errorf("either token-file, token or no-auth need to be specified")
+	}
+
+	var tokenEntries = make(map[string]string)
+	var tokenValue []string
+
+	for _, token := range *tokens {
+		tokenValue = strings.Split(token, ":")
+		if len(tokenValue) != 2 {
+			return fmt.Errorf("invalid token format %v", token)
+		}
+		tokenEntries[tokenValue[1]] = tokenValue[0]
 	}
 
 	var resetTimer *time.Timer
 
 	http.HandleFunc("/api/open", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("open request")
+		if *noAuthRequired {
+			log.Printf("open request")
+		} else {
+			if tokenName, ok := tokenEntries[r.Header.Get("X-Token")]; ok {
+				log.Printf("open request for %v", tokenName)
+			} else {
+				http.Error(w, "invalid token", http.StatusForbidden)
+				return
+			}
+		}
 		gpio.SetTrigger(true)
 		fmt.Fprintf(w, "ok")
 		if resetTimer != nil {
@@ -66,15 +111,21 @@ func runIt() {
 	})
 
 	http.HandleFunc("/api/check", func(w http.ResponseWriter, r *http.Request) {
+		if !*noAuthRequired {
+			if _, ok := tokenEntries[r.Header.Get("X-Token")]; !ok {
+				http.Error(w, "invalid token", http.StatusForbidden)
+				return
+			}
+		}
 		fmt.Fprintf(w, "%v", gpio.ReadFeedback())
 	})
 
 	http.Handle("/", http.FileServer(assets.Assets))
 	log.Println("Listening on http://" + *bindAddress)
-	log.Fatal(http.ListenAndServe(*bindAddress, nil))
+	return http.ListenAndServe(*bindAddress, nil)
 }
 
 func main() {
 	flag.Parse()
-	runIt()
+	log.Fatal(runIt())
 }
--- main_test.go
+++ main_test.go
@@ -6,29 +6,46 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"syscall"
 	"testing"
 	"time"
 
 	"github.com/fopina/rgrweb/helpers"
 )
 
-func ExampleTestInput() {
+func assertError(t *testing.T, err error, message string) {
+	if err.Error() != message {
+		t.Fatalf("unexpected err: %v", err)
+	}
+}
+
+func reset() {
 	log.SetOutput(os.Stdout)
 	log.SetFlags(0)
-	helpers.SetFakeTrigger(false)
-	*testInput = true
+	// needed to reset HandleFunc registry across tests
+	http.DefaultServeMux = http.NewServeMux()
+
+	*tokenFile = ""
+	*tokens = []string{}
+	*noAuthRequired = false
+	*testInput = false
 	*testOutput = false
-	*highDuration = 1 * time.Millisecond
+	helpers.SetFakeTrigger(false)
+}
+
+func ExampleTestInput() {
+	reset()
+	*testInput = true
+	*highDuration = 6 * time.Millisecond
 	go runIt()
-	time.Sleep(2 * time.Millisecond)
+	time.Sleep(9 * time.Millisecond)
 	// Output:
 	// Use Ctrl-C to stop...
-	// GPIO0=false
 	// GPIO0=false
 }
 
 func TestTestOutput(t *testing.T) {
-	*testInput = false
+	reset()
 	*testOutput = true
 	*highDuration = 10 * time.Millisecond
 	if helpers.ReadFakeFeedback() {
@@ -45,27 +62,66 @@ func TestTestOutput(t *testing.T) {
 	}
 }
 
-func TestRunIt(t *testing.T) {
-	*testInput = false
-	*testOutput = false
-	*bindAddress = "127.0.0.1:9999"
-	helpers.SetFakeTrigger(false)
-	go runIt()
+func TestRunIt_TokenAndAuth(t *testing.T) {
+	reset()
+	// hacky way to stop runIt for "succesful auth setup" == make it break after the checks
+	*bindAddress = "invalid"
+	err := runIt()
+	assertError(t, err, "either token-file, token or no-auth need to be specified")
+	reset()
+	*bindAddress = "invalid"
+	*noAuthRequired = true
+	err = runIt()
+	assertError(t, err, "listen tcp: address invalid: missing port in address")
+	reset()
+	f, err := ioutil.TempFile("", "testtokenfile")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer syscall.Unlink(f.Name())
+	ioutil.WriteFile(f.Name(), []byte("a:12345\nb:54321"), 0644)
+	*bindAddress = "invalid"
+	*tokenFile = f.Name()
+	err = runIt()
+	assertError(t, err, "listen tcp: address invalid: missing port in address")
+	reset()
+	*bindAddress = "invalid"
+	*tokens = []string{"asd"}
+	err = runIt()
+	assertError(t, err, "invalid token format asd")
+	*tokens = []string{"name:token"}
+	err = runIt()
+	assertError(t, err, "listen tcp: address invalid: missing port in address")
+}
 
-	var resp *http.Response
+func waitForIt(t *testing.T) {
 	var err error
 
 	for {
 		// FIXME: how to properly wait for main() to get ready without modifying it...?
 		// gief python monkeypatching power
-		resp, err = http.Get("http://127.0.0.1:9999/api/check")
+		_, err = http.Get("http://127.0.0.1:9999/")
 		if err == nil {
-			break
+			return
 		}
 		if !strings.Contains(err.Error(), "connection refused") {
-			t.Fatalf("failed: %v", err)
+			t.Fatal(err)
 		}
-		t.Logf("main() not ready yet...")
+		t.Log("http server not ready yet...")
+	}
+}
+
+func TestRunIt_NoAuth(t *testing.T) {
+	reset()
+	*bindAddress = "127.0.0.1:9999"
+	*noAuthRequired = true
+	go runIt()
+
+	waitForIt(t)
+
+	resp, err := http.Get("http://127.0.0.1:9999/api/check")
+	if err != nil {
+		t.Fatalf("failed: %v", err)
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
@@ -104,4 +160,130 @@ func TestRunIt(t *testing.T) {
 	if string(body) != "true" {
 		t.Fatalf("check should be true after open: %v", string(body))
 	}
+	// hacky, make sure timer runs out and log is completed
+	time.Sleep(*highDuration)
+}
+
+func TestRunIt_WithBadAuth(t *testing.T) {
+	reset()
+	*bindAddress = "127.0.0.1:9999"
+	*tokens = []string{"a:12345", "b:98765"}
+	go runIt()
+
+	waitForIt(t)
+
+	resp, err := http.Get("http://127.0.0.1:9999/api/check")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("failed: %v", err)
+	}
+
+	if string(body) != "invalid token\n" {
+		t.Fatalf("/api/check without token did not return error: %v", string(body))
+	}
+
+	resp, err = http.Get("http://127.0.0.1:9999/api/open")
+	if err != nil {
+		t.Fatalf("failed: %v", err)
+	}
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("failed: %v", err)
+	}
+
+	if string(body) != "invalid token\n" {
+		t.Fatalf("/api/open without token did not return error: %v", string(body))
+	}
+
+	if helpers.ReadFakeFeedback() {
+		t.Fatalf("gpio should not be true after invalid open request")
+	}
+
+	// try just one more time with an actual token but invalid
+	req, err := http.NewRequest("GET", "http://127.0.0.1:9999/api/check", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("X-Token", "1234")
+	resp, err = http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatalf("failed: %v", err)
+	}
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("failed: %v", err)
+	}
+	if string(body) != "invalid token\n" {
+		t.Fatalf("/api/check with invalid token did not return error: %v", string(body))
+	}
+}
+
+func TestRunIt_WithGoodAuth(t *testing.T) {
+	reset()
+	*bindAddress = "127.0.0.1:9999"
+	*tokens = []string{"a:12345", "b:98765"}
+	go runIt()
+
+	waitForIt(t)
+
+	req, err := http.NewRequest("GET", "http://127.0.0.1:9999/api/check", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("X-Token", "12345")
+	resp, err := http.DefaultClient.Do(req)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("failed: %v", err)
+	}
+
+	if string(body) != "false" {
+		t.Errorf("check should be false at start: %v", string(body))
+	}
+
+	req, err = http.NewRequest("GET", "http://127.0.0.1:9999/api/open", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("X-Token", "12345")
+	resp, err = http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatalf("failed: %v", err)
+	}
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("failed: %v", err)
+	}
+
+	if string(body) != "ok" {
+		t.Fatalf("open not ok: %v", string(body))
+	}
+
+	req, err = http.NewRequest("GET", "http://127.0.0.1:9999/api/check", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("X-Token", "12345")
+	resp, err = http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatalf("failed: %v", err)
+	}
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("failed: %v", err)
+	}
+
+	if string(body) != "true" {
+		t.Fatalf("check should be true after open: %v", string(body))
+	}
+	// hacky, make sure timer runs out and log is completed
+	time.Sleep(*highDuration)
 }
